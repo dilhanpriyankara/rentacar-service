be6b5a8e5a4f66d0310163fa59edb734c53ff381
DESKTOP-2SET90H\dilhan <gdpa16@gmail.com>
2025-11-06 10:44:31 +0530

    initial commit

---
+++ src/main/java/com/rentacar/rentacar_service/RentacarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rentacar.rentacar_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentacar/rentacar_service/RentacarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentacar.rentacar_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
